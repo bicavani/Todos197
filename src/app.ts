c5b9c825bee87f2c38a3ceed03e3043cab5a25d4
Namlongcomputer <hoandm91@gmail.com>
2020-10-30 23:34:16 +0700

    abc

--- src/app.ts
+++ src/app.ts
@@ -11,9 +11,18 @@ const PORT: string | number = process.env.PORT || 4000
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cors())
-app.use(todoRoutes)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"))
+app.use(todoRoutes)
+
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World!</h1>")
+  })
+
+app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`))
 
 const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.0odm4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
@@ -26,9 +35,6 @@ mongoose.set('useFindAndModify', true);
 mongoose.connect(url,connectionParams)
     .then( () => {
         console.log('Connected to database ')
-        app.listen(PORT, () =>
-          console.log(`Server running on http://localhost:${PORT}`)
-        )
     })
     .catch( (err) => {
         console.error(`Error connecting to the database. \n${err}`);
