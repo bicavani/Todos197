1f9ee4fd73fc8b0f5b6686c4fbda8d3029e29da4
Namlongcomputer <hoandm91@gmail.com>
2020-11-12 14:53:45 +0700

    done app

--- dist/js/server.js
+++ dist/js/server.js
@@ -10,16 +10,20 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const todo_route_1 = __importDefault(require("./routes/todo.route"));
 const user_route_1 = __importDefault(require("./routes/user.route"));
 const auth_middleware_1 = require("./middleware/auth.middleware");
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use(cors_1.default());
 app.use('/todos', auth_middleware_1.auth, todo_route_1.default);
 app.use('/user', user_route_1.default);
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express_1.default.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path_1.default.join(__dirname, "client", "build", "index.html"));
+    });
+}
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.0odm4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 const connectionParams = {
--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,11 @@ import bodyParser from 'body-parser';
 import todoRoutes from './routes/todo.route';
 import userRoutes from './routes/user.route';
 import { auth } from './middleware/auth.middleware';
+import path from 'path'
 
 const app: Express = express();
 
-const PORT: string | number = process.env.PORT || 4000;
+const PORT: string | number = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,9 +19,13 @@ app.use(cors());
 app.use('/todos', auth, todoRoutes);
 app.use('/user', userRoutes);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
