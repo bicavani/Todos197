3086a775450a23d1a4d2aec4f5d58cf52dcaa125
Namlongcomputer <hoandm91@gmail.com>
2020-11-12 15:34:51 +0700

    fix

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,14 @@ app.use(cors());
 app.use('/todos', auth, todoRoutes);
 app.use('/user', userRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
 const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.0odm4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
